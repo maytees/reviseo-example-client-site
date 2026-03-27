0fd174df45c1c89ec56129dbf6b0fcd6acc18619
Maytham Ajam <mateespublicprofile@gmail.com>
2026-03-27 02:16:02 -0400

    fix: reviseo snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Raleway, Manrope } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
+import Script from "next/script"
 
 const manropeHeading = Manrope({
   subsets: ["latin"],
@@ -35,6 +36,18 @@ export default function RootLayout({
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
+        <Script
+        onLoad={() => {
+          window.ReviseoConfig = {projectId: "6b6046dc-5750-4b3a-a5b8-113804dfb9b2"},
+          function (e, t) {
+            if (e.__Reviseo) return;
+            e.__Reviseo = {};
+            const i = t.createElement("script");
+            i.src = "https://www.reviseo.app/reviseo.js";
+            const n = t.getElementsByTagName("script")[0];
+            n.parentNode.insertBefore(i, n)
+          }(window, document);
+          }/>
       </body>
     </html>
   )
