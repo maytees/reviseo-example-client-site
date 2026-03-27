9b33e6159645f6fc22e9c4f35d921ec51a7cde66
Maytham Ajam <mateespublicprofile@gmail.com>
2026-03-27 02:16:46 -0400

    fix: syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,17 +37,20 @@ export default function RootLayout({
       <body>
         <ThemeProvider>{children}</ThemeProvider>
         <Script
-        onLoad={() => {
-          window.ReviseoConfig = {projectId: "6b6046dc-5750-4b3a-a5b8-113804dfb9b2"},
-          function (e, t) {
-            if (e.__Reviseo) return;
-            e.__Reviseo = {};
-            const i = t.createElement("script");
-            i.src = "https://www.reviseo.app/reviseo.js";
-            const n = t.getElementsByTagName("script")[0];
-            n.parentNode.insertBefore(i, n)
-          }(window, document);
-          }/>
+          onLoad={() => {
+            ;((window.ReviseoConfig = {
+              projectId: "6b6046dc-5750-4b3a-a5b8-113804dfb9b2",
+            }),
+              (function (e, t) {
+                if (e.__Reviseo) return
+                e.__Reviseo = {}
+                const i = t.createElement("script")
+                i.src = "https://www.reviseo.app/reviseo.js"
+                const n = t.getElementsByTagName("script")[0]
+                n.parentNode.insertBefore(i, n)
+              })(window, document))
+          }}
+        />
       </body>
     </html>
   )
